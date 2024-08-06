ab31edd322cc2acacd5da3784776286512265c62
M Paula <paulla.ps83@gmail.com>
2024-08-06 11:18:30 -0300

    Corrigido erros de Deploy

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,22 +8,13 @@ import { PostagemModule } from './postagem/postagem.module';
 import { TemaModule } from './tema/tema.module';
 import { UsuarioModule } from './usuario/usuario.module';
 import { DevService } from './data/services/dev.service';
-import { Usuario } from './usuario/entities/usuario.entity';
-import { Postagem } from './postagem/entities/postagem.entity';
-import { Tema } from './tema/entities/tema.entity';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: "mysql",
-      host: "localhost",
-      port: 3306,
-      username: "root",
-      password: "root",
-      database: "db_blogpessoal",
-      entities: [Postagem, Tema, Usuario],
-      synchronize: true,
-    
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRootAsync({
+      useClass: ProdService,
+      imports: [ConfigModule],
     }),
     PostagemModule,
     TemaModule,
