04d5ab0448bbb291a9c6ef9c7c023d1e519c1b99
M Paula <paulla.ps83@gmail.com>
2024-08-06 11:16:19 -0300

    Adicionado Testes

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,13 +8,22 @@ import { PostagemModule } from './postagem/postagem.module';
 import { TemaModule } from './tema/tema.module';
 import { UsuarioModule } from './usuario/usuario.module';
 import { DevService } from './data/services/dev.service';
+import { Usuario } from './usuario/entities/usuario.entity';
+import { Postagem } from './postagem/entities/postagem.entity';
+import { Tema } from './tema/entities/tema.entity';
 
 @Module({
   imports: [
-    ConfigModule.forRoot(),
-    TypeOrmModule.forRootAsync({
-      useClass: ProdService,
-      imports: [ConfigModule],
+    TypeOrmModule.forRoot({
+      type: "mysql",
+      host: "localhost",
+      port: 3306,
+      username: "root",
+      password: "root",
+      database: "db_blogpessoal",
+      entities: [Postagem, Tema, Usuario],
+      synchronize: true,
+    
     }),
     PostagemModule,
     TemaModule,
