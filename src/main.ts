4199a8544e88cf2e7bf90134e1b37f8818ac91e7
M Paula <paulla.ps83@gmail.com>
2024-08-05 23:15:00 -0300

    Deploy Projeto Blog Pessoal

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,25 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  const config = new DocumentBuilder()
+  .setTitle('Blog Pessoal')
+  .setDescription('Projeto Blog Pessoal')
+  .setContact("Maria Paula","https://github.com/mpaulas","paulla.ps83@gmail.com")
+  .setVersion('1.0')
+  .addBearerAuth()
+  .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('/swagger', app, document);
+  
   process.env.TZ = '-03:00'; // Time Zone Brasil
 
   app.useGlobalPipes(new ValidationPipe());
   app.enableCors();
-  await app.listen(4000);
+  await app.listen(process.env.PORT || 4000);
 }
 bootstrap();
--- src/postagem/entities/postagem.entity.ts
+++ src/postagem/entities/postagem.entity.ts
@@ -1,34 +1,40 @@
 import { Transform, TransformFnParams } from "class-transformer";
 import { IsNotEmpty } from "class-validator";
-import { Column, Entity, ManyToMany, ManyToOne, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
 import { Tema } from "../../tema/entities/tema.entity";
 import { Usuario } from "../../usuario/entities/usuario.entity";
+import { ApiProperty } from "@nestjs/swagger";
 
 @Entity({ name: "tb_postagens"}) // Se não acrescentar essa entidade, o código vai gerar o nome da tabela igual o da classe. Cria tabela
 export class Postagem{
 
+    @ApiProperty()
     @PrimaryGeneratedColumn() // Decorador sempre em cima do atribudo, nesse caso id, Gera chave primária autoincremental.
     id: number;
 
     @Transform(({ value }: TransformFnParams) => value?.trim()) // Bloquear apenas espaços em branco
 
+    @ApiProperty()
     @IsNotEmpty() // Não aceita titulo vazio
     @Column({length: 100, nullable: false}) // Define tamanho e não aceita null
     titulo: string;
 
+    @ApiProperty()
     @IsNotEmpty()
     @Column({length: 1000, nullable: false})
     texto: string;
 
+    @ApiProperty()
     @UpdateDateColumn() // A data e hora são preenchidos automaticamente
     data: Date;
 
-    // Muitos para Um, ou seja, muitas postagens possuem um tema
-    @ManyToOne(() => Tema, (tema) => tema.postagem,{
+    @ApiProperty()
+    @ManyToOne(() => Tema, (tema) => tema.postagem,{ // Muitos para Um, ou seja, muitas postagens possuem um tema
         onDelete: "CASCADE"
     })
     tema: Tema;
     
+    @ApiProperty()
     @ManyToOne(() => Usuario, (usuario) => usuario.postagem,{
         onDelete: "CASCADE"
     })
